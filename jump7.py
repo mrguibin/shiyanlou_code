76a120a6f2db656b8515afee76a719a7fcf55a8c
mrguibin <https://github.com/mrguibin/shiyanlou_code.git>
2020-04-30 15:36:07 +0800

    feng 7 guo

---
+++ jump7.py
@@ -0,0 +1,13 @@
+for a in range(1,101):
+        if a % 7  == 0:
+                    continue
+                        #print('你遇到了{}，过'.format(a))
+                            elif a % 10 == 7:
+                                        continue
+                                            #print('你遇到了{}，过'.format(a))
+                                                elif a // 10 == 7:
+                                                            continue
+                                                                #print('你遇到了{}，过'.format(a))
+                                                                    else:
+                                                                                print(a)
+
